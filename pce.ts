a02b2c3bb372b7fdd3c7efd82349298d6362f1c8
unknown <timeo.perrinjaquet@gmail.com>
2023-02-28 00:08:47 +0100

    Translated up to get_moves

--- pce.ts
+++ pce.ts
@@ -7,7 +7,7 @@
 */
 
 namespace PlayeChessEngine {
-    class Move {
+    export class Move {
         private StartCoords: Array<number>;
         public get start_coords() : Array<number> {
             return this.StartCoords;
@@ -34,11 +34,9 @@ namespace PlayeChessEngine {
             return this.IsValid;
         }
 
-        constructor(start_square_x: number, start_square_y: number, end_square_x: number, end_square_y: number, is_capture: boolean, is_valid: boolean) {
+        constructor(start_square_x: number, start_square_y: number, end_square_x: number, end_square_y: number) {
             this.StartCoords = [start_square_x, start_square_y];
             this.EndCoords = [end_square_x, end_square_y];
-            this.is_capture = is_capture;
-            this.is_valid = is_valid;
         }
 
         public show() : string {
@@ -53,7 +51,7 @@ namespace PlayeChessEngine {
 
             enum piece_type { p, r, n, b, q, k };
 
-            abstract class Piece {
+            abstract export class Piece {
                 private Type: piece_type;
                 protected get type(): piece_type {
                     return this.Type;
@@ -173,9 +171,16 @@ namespace PlayeChessEngine {
                     let piece_names: Array<string> = ["p", "n", "b", "r", "q", "k"];
                     return piece_names[this.type];
                 }
+
+                public compare(other: Piece): boolean {
+                    if(this.type == other.type && this.is_white == other.is_white && this.coords[0] == other.coords[0] && this.coords[1] == other.coords[1]) {
+                        return true;
+                    }
+                    return false;
+                }
             }
 
-            class Pawn extends Piece {
+            export class Pawn extends Piece {
                 constructor(is_white: boolean, x: number, y: number) {
                     super(piece_type.p, is_white, x, y);
                 }
@@ -232,15 +237,245 @@ namespace PlayeChessEngine {
                 }
             }
 
-            
+            export class Rook extends Piece {
+                constructor(is_white: boolean, x: number, y: number) {
+                    super(piece_type.r, is_white, x, y);
+                }
+
+                validation_function(board: [[Piece, Piece, Piece, Piece, Piece, Piece, Piece, Piece], [Piece, Piece, Piece, Piece, Piece, Piece, Piece, Piece], [Piece, Piece, Piece, Piece, Piece, Piece, Piece, Piece], [Piece, Piece, Piece, Piece, Piece, Piece, Piece, Piece], [Piece, Piece, Piece, Piece, Piece, Piece, Piece, Piece], [Piece, Piece, Piece, Piece, Piece, Piece, Piece, Piece], [Piece, Piece, Piece, Piece, Piece, Piece, Piece, Piece], [Piece, Piece, Piece, Piece, Piece, Piece, Piece, Piece]], x_final: number, y_final: number): boolean {
+                    let x_diff: number = x_final - this.coords[0];
+                    let y_diff: number = y_final - this.coords[1];
+                    if(x_diff * y_diff == 0 && x_diff != y_diff) {
+                        if(this.validate_validation(board, x_final, y_final)) {
+                            return this.check_path(board, x_final, y_final);
+                        }
+                    }
+                    return false;
+                }
+            }
+
+            export class Knight extends Piece {
+                constructor(is_white: boolean, x: number, y: number) {
+                    super(piece_type.n, is_white, x, y);
+                }
+
+                public validation_function(board: [[Piece, Piece, Piece, Piece, Piece, Piece, Piece, Piece], [Piece, Piece, Piece, Piece, Piece, Piece, Piece, Piece], [Piece, Piece, Piece, Piece, Piece, Piece, Piece, Piece], [Piece, Piece, Piece, Piece, Piece, Piece, Piece, Piece], [Piece, Piece, Piece, Piece, Piece, Piece, Piece, Piece], [Piece, Piece, Piece, Piece, Piece, Piece, Piece, Piece], [Piece, Piece, Piece, Piece, Piece, Piece, Piece, Piece], [Piece, Piece, Piece, Piece, Piece, Piece, Piece, Piece]], x_final: number, y_final: number): boolean {
+                    let x_diff: number = x_final - this.coords[0];
+                    let y_diff: number = y_final - this.coords[1];
+                    if((Math.abs(x_diff) == 2 && Math.abs(y_diff) == 1) || (Math.abs(x_diff) == 1 && Math.abs(y_diff) == 2)) {
+                        return this.validate_validation(board, x_final, y_final);
+                    }
+                    return false;
+                }
+            }
+
+            export class Bishop extends Piece {
+                constructor(is_white: boolean, x: number, y: number) {
+                    super(piece_type.b, is_white, x, y);
+                }
+
+                public validation_function(board: [[Piece, Piece, Piece, Piece, Piece, Piece, Piece, Piece], [Piece, Piece, Piece, Piece, Piece, Piece, Piece, Piece], [Piece, Piece, Piece, Piece, Piece, Piece, Piece, Piece], [Piece, Piece, Piece, Piece, Piece, Piece, Piece, Piece], [Piece, Piece, Piece, Piece, Piece, Piece, Piece, Piece], [Piece, Piece, Piece, Piece, Piece, Piece, Piece, Piece], [Piece, Piece, Piece, Piece, Piece, Piece, Piece, Piece], [Piece, Piece, Piece, Piece, Piece, Piece, Piece, Piece]], x_final: number, y_final: number): boolean {
+                    let x_diff: number = x_final - this.coords[0];
+                    let y_diff: number = y_final - this.coords[1];
+                    if(Math.abs(x_diff) == Math.abs(y_diff) && x_diff != 0) {
+                        if(this.validate_validation(board, x_final, y_final)) {
+                            return this.check_path(board, x_final, y_final);
+                        }
+                    }
+                    return false;
+                }
+            }
+
+            export class Queen extends Piece {
+                constructor(is_white: boolean, x: number, y: number) {
+                    super(piece_type.q, is_white, x, y);
+                }
+
+                public validation_function(board: [[Piece, Piece, Piece, Piece, Piece, Piece, Piece, Piece], [Piece, Piece, Piece, Piece, Piece, Piece, Piece, Piece], [Piece, Piece, Piece, Piece, Piece, Piece, Piece, Piece], [Piece, Piece, Piece, Piece, Piece, Piece, Piece, Piece], [Piece, Piece, Piece, Piece, Piece, Piece, Piece, Piece], [Piece, Piece, Piece, Piece, Piece, Piece, Piece, Piece], [Piece, Piece, Piece, Piece, Piece, Piece, Piece, Piece], [Piece, Piece, Piece, Piece, Piece, Piece, Piece, Piece]], x_final: number, y_final: number): boolean {
+                    let x_diff: number = x_final - this.coords[0];
+                    let y_diff: number = y_final - this.coords[1];
+                    if((Math.abs(x_diff) == Math.abs(y_diff) && x_diff != 0) || (x_diff * y_diff == 0 && x_diff != y_diff)) {
+                        if(this.validate_validation(board, x_final, y_final)) {
+                            return this.check_path(board, x_final, y_final);
+                        }
+                    }
+                    return false;
+                }
+            }
+
+            export class King extends Piece {
+                constructor(is_white: boolean, x: number, y: number) {
+                    super(piece_type.k, is_white, x, y);
+                }
+
+                public validation_function(board: [[Piece, Piece, Piece, Piece, Piece, Piece, Piece, Piece], [Piece, Piece, Piece, Piece, Piece, Piece, Piece, Piece], [Piece, Piece, Piece, Piece, Piece, Piece, Piece, Piece], [Piece, Piece, Piece, Piece, Piece, Piece, Piece, Piece], [Piece, Piece, Piece, Piece, Piece, Piece, Piece, Piece], [Piece, Piece, Piece, Piece, Piece, Piece, Piece, Piece], [Piece, Piece, Piece, Piece, Piece, Piece, Piece, Piece], [Piece, Piece, Piece, Piece, Piece, Piece, Piece, Piece]], x_final: number, y_final: number): boolean {
+                    let x_diff: number = x_final - this.coords[0];
+                    let y_diff: number = y_final - this.coords[1];
+                    if(Math.abs(x_diff) <= 1 && Math.abs(y_diff) <= 1) {
+                        return this.validate_validation(board, x_final, y_final);
+                    }
+                    return false;
+                }
+            }
+        } // namespace pieces
+
+        export class Board {
+            private Board: [[pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece], [pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece], [pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece], [pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece], [pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece], [pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece], [pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece], [pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece]];
+            public set board(board: [[pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece], [pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece], [pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece], [pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece], [pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece], [pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece], [pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece], [pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece]]) {
+                this.Board = board;
+            }
+            public get board(): [[pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece], [pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece], [pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece], [pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece], [pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece], [pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece], [pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece], [pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece]] {
+                return this.Board;
+            }
+
+            private Moves: Array<Move>;
+            public set moves(moves: Array<Move>) {
+                this.Moves = moves;
+            }
+            public get moves(): Array<Move> {
+                return this.Moves;
+            }
+
+            private WhiteTurn: boolean = true;
+            public set white_turn(whiteTurn: boolean) {
+                this.WhiteTurn = whiteTurn;
+            }
+            public get white_turn(): boolean {
+                return this.WhiteTurn;
+            }
+
+            constructor(fen:string = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1") {
+                let fen_array: Array<string> = fen.split(" ");
+                fen = fen_array[0];
+                this.white_turn = fen_array[1] == "w";
+                this.load_fen(fen);
+            }
+
+            public compare(other: Board): boolean {
+                for(let i = 0; i < 8; i++) {
+                    for(let j = 0; j < 8; j++) {
+                        if(this.Board[i][j].compare(other.Board[i][j])) {
+                            return false;
+                        }
+                    }
+                }
+                if(this.white_turn != other.white_turn) {
+                    return false;
+                }
+                // come bak here
+                return true;
+            }
+
+            public reverse_fen(fen: string): string {
+                let fen_array: Array<string> = fen.split("/");
+                let new_fen: string = "";
+                for(let i = fen_array.length - 1; i >= 0; i--) {
+                    new_fen += [...fen_array[i]].reverse().join('') + "/";
+                }
+                return new_fen;
+            }
+
+            public load_fen(fen: string): void {
+                fen = this.reverse_fen(fen);
+                let fen_arr = [...fen];
+                let x: number = 0;
+                let y: number = 0;
+                fen_arr.forEach(c => {
+                    if(c == "/") {
+                        y = 0;
+                        x++;
+                    } else if(c == "8")
+                        return;
+                    else if(c >= "1" && c <= "8") {
+                        y += c.charCodeAt(0) - "0".charCodeAt(0);
+                    } else {
+                        let is_white: boolean = c >= "A" && c <= "Z";
+                        switch(c) {
+                            case "P":
+                            case "p":
+                                this.Board[x][y] = new pieces.Pawn(is_white, x, y);
+                                break;
+                            case "R":
+                            case "r":
+                                this.Board[x][y] = new pieces.Rook(is_white, x, y);
+                                break;
+                            case "N":
+                            case "n":
+                                this.Board[x][y] = new pieces.Knight(is_white, x, y);
+                                break;
+                            case "B":
+                            case "b":
+                                this.Board[x][y] = new pieces.Bishop(is_white, x, y);
+                                break;
+                            case "Q":
+                            case "q":
+                                this.Board[x][y] = new pieces.Queen(is_white, x, y);
+                                break;
+                            case "K":
+                            case "k":
+                                this.Board[x][y] = new pieces.King(is_white, x, y);
+                                break;
+                        }
+                        y++;
+                    }
+                });
+            }
+
+            public print_board(moves: Array<Array<number>> =[], board: [[pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece], [pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece], [pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece], [pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece], [pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece], [pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece], [pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece], [pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece, pieces.Piece]] = this.Board): void {
+                let board_str: string = "";
+                board_str += "  #-----------------#\n";
+                for(let i = 0; i < 8; i++) {
+                    board_str += (8 - i) + " | ";
+                    for(let j = 0; j < 8; j++) {
+                        let skip: boolean = false;
+                        for(let k = 0; k < moves.length; k++) {
+                            if(moves[k][0] == 7 - i && moves[k][1] == j) {
+                                board_str += "* ";
+                                skip = true;
+                                break;
+                            }
+                        }
+                        if(skip) continue;
+                        if(board[7 - i][j] == null) {
+                            board_str += "  ";
+                        } else {
+                            board_str += board[7 - i][j].show() + " ";
+                        }
+                    }
+                    board_str += "|\n";
+                }
+                board_str += "  #-----------------#\n";
+                board_str += "    a b c d e f g h\n";
+                console.log(board_str);
+            }
+
+            public get_moves(x: number, y: number, from_premove:boolean = false): Array<Move> {
+                let moves: Array<Move> = [];
+                for(let i = 0; i < 8; i++) {
+                    for(let j = 0; j < 8; j++) {
+                        if(this.Board[i][j] == null) continue;
+                        if(this.Board[i][j].is_white != this.white_turn) continue;
+                        if(this.board[x][y].validation_function(this.board, i, j)) {
+                            let move: Move = new Move(i, j, x, y);
+                            if(from_premove) {
+                                moves.push(move);
+                                continue;
+                            }
+                            if(!this.premove_check(move, this.board[x][y].is_white))
+                                moves.push(move);
+                        }
+                    }
+                }
+                return moves;
+            }
         }
-    }
-}
+    } // namespace board
+} // namespace PlayeChessEngine
 
 /**
  * Fonctionnement
  * 
- * Classe PCE:
+ * export classe PCE:
  * 
  * isDraw() -> [bool, (type: string)]
  * isLegal() -> [bool, (board: array 2D)]
